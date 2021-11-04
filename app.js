7056486a5a1b4cc718133aa65278a388ab275a95
unknown <you@example.com>
2021-11-04 17:49:06 +0100

    creat user modul and db connect

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connectDB = require('./db/connect');
+require('dotenv').config()
+const app = express();
+
+// app use json parce
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// starte server function
+const start = async ()=>{
+    await connectDB(process.env.HOST_NAME);
+    app.listen(5000,()=>{
+        console.log('your server is up in port 5000...');
+    })
+}
+start();
---
+++ models/userModule.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserShema = mongoose.Schema({
+  email: {
+    type: String,
+    require: [true, "email is require"],
+  },
+  password: {
+    type: String,
+    require: [true, "password is require"],
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('UserModule',UserShema)
